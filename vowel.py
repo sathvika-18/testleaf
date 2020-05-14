269b6eb607dbe998d993eb2fb87520998385c3ec
sathvika-18 <64521616+sathvika-18@users.noreply.github.com>
2020-05-14 16:49:44 +0530

    it will print the count of vowel and consonents

---
+++ vowel.py
@@ -0,0 +1,10 @@
+a='python is a easy language'  
+vowel=0
+consonent=0
+for i in a:
+	if(i=='a'or i=='e'or i=='i'or i=='o'or i=='u'):
+		vowel=vowel+1
+	else:
+	    consonent=consonent+1
+print("vowel count",vowel)
+print("consonent count",consonent)	    
